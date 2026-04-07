ee5db02a3fbf90f44fbbbdb7d57dbfbb83e00b40
7diegojr <dfsntsjunior@gmail.com>
2026-04-07 11:54:55 -0300

    Finalizado o Layout Padrão, a Partial View _LoginPartial e o Estilo Global e JavaScript da Navbar

--- ToDoPlatform/wwwroot/js/site.js
+++ ToDoPlatform/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const userMenu = document.getElementById('userMenu');
+const dropdown = document.getElementById('dropdown');
 
-// Write your JavaScript code.
+/* ao clicar no menu do usuário, a classe active é
+alternada no dropdown com classList.toggle() */
+userMenu.addEventListener('click', (e) => {
+    e.stopPropagation(); /* impede que o clique se propague para o document */
+    dropdown.classList.toggle('active');
+});
+
+document.addEventListener('click', () => {
+    /* Qualquer clique fora do menu remove a classe active, fechando o dropdown. */
+    dropdown.classList.remove('active');
+});
